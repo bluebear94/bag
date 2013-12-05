8e24a3af70c5440b65c5b018670b218a8aeb8139
Tae Lim Kook <tkook11@gmail.com>
2013-12-05 07:50:45 -0500

    Finished util/BigIntOps.scala (didn't have time yesterday)

--- src/util/BigIntOps.scala
+++ src/util/BigIntOps.scala
@@ -1,5 +1,18 @@
 package util
 
 import java.math.BigInteger
+import types.TMountain
 
-def fits(x: Type, y: Type, p: (Int) => Boolean)
+object BigIntOps {
+  def fits(x: BigInteger, y: BigInteger, p: (Int) => Boolean) = {
+	p(x.compareTo(y))
+  }
+  def makeFF(p: (Int) => Boolean): (BigInteger, BigInteger) => Boolean = {
+    (x: BigInteger, y: BigInteger) =>
+      fits(x, y, p)
+  }
+  val gt = makeFF((x) => x > 0)
+  val lt = makeFF((x) => x < 0)
+  val ge = makeFF((x) => x >= 0)
+  val le = makeFF((x) => x <= 0)
+}
