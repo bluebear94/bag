342abdaaf2a680682f18688df46afa4aed01cee0
Tae Lim Kook <tkook11@gmail.com>
2014-05-22 18:45:53 -0400

    Removed pre-SBT fluff

--- src/cmdreader/Command.scala
+++
@@ -1,8 +0,0 @@
-package cmdreader
-import types._
-
-abstract class Command {
-  def getName(): String = ""
-  def isValidArg0(n: Int): Boolean = true
-  def apply(args: Array[Type]): Type = new TVoid()
-}
--- src/types/TNumerical.scala
+++
@@ -1,9 +0,0 @@
-package types
-
-abstract class TNumerical extends Type {
-  def getVal(): Any
-  def toBoolean(): Boolean = {
-    getVal() == 0
-  }
-  def intValue(): Int
-}
