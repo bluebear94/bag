15a3769d859b0851c8bf9c765e8170cddf7a1ea2
Tae Lim Kook <tkook11@gmail.com>
2014-07-12 23:09:32 -0400

    Fixed ClassCastException

--- src/main/scala/cmdreader/std/ClrHome.scala
+++ src/main/scala/cmdreader/std/ClrHome.scala
@@ -3,7 +3,7 @@ package cmdreader.std
 import types._
 import gui.Main
 
-class ClrHome {
+class ClrHome extends Command {
   def getName(): String = "clrHome"
   def isValidArg0(n: Int): Boolean = n == 0
   def apply(args: Array[Type]): Type = {
